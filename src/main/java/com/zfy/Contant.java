6bde15d588fb0ddf2c74f1574254407f9f4ceeac
zhangfeiyang <zfy15801121351@163.com>
2020-01-10 17:03:31 +0800

    测试

---
+++ src/main/java/com/zfy/Contant.java
@@ -0,0 +1,26 @@
+package com.zfy;
+
+public class Contant {
+    public static String[] JOINTS = new String[]{
+            "Neck",
+            "SpineShoulder_Mid",
+            "SpineShoulder_Right",
+            "SpineShoulder_Left",
+            "SpineMid",
+            "SpineBase_Right",
+            "SpineBase_Left",
+            "WristRight",
+            "ElbowRight",
+            "ShoulderRight",
+            "HipRight",
+            "KneeRight",
+            "AnkleRight",
+            "WristLeft",
+            "ElbowLeft",
+            "ShoulderLeft",
+            "HipLeft",
+            "KneeLeft",
+            "AnkleLeft"
+            //测试
+    };
+}
